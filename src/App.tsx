73d56c92af256db911674fae0229bda88fb5878a
LokWenZel <p22014658@student.newinti.edu.my>
2025-12-17 10:23:42 +0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  const items = ["New York", "San Francisco", "Tokyo", "Paris"];
+
+  items.map(item => <li></li>)
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
